2f20fc602cc9fb5971085f3343a872f93f57e429
Dabawsl24l <jordandallaire23@gmail.com>
2023-10-29 14:14:15 -0400

    Ajout de la fonction qui détecte le sol
    Pour gérer les mouvement si le joueur touche le sol ou pas

--- Assets/Scripts/VoitureJoueur.cs
+++ Assets/Scripts/VoitureJoueur.cs
@@ -7,6 +7,8 @@ public class VoitureJoueur : MonoBehaviour
 
     public float repoussement = 3000;
 
+    public bool isOnGround;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,13 +23,30 @@ public class VoitureJoueur : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-    if (collision.gameObject.tag == "Player")
+
+        if (collision.gameObject.tag == "Player")
+            {
+
+                Rigidbody autreRb = collision.rigidbody;
+
+                autreRb.AddExplosionForce(repoussement, collision.contacts[0].point, 5);
+            
+            }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Sol"))
         {
+            isOnGround = true;
+        }
+    }
 
-            Rigidbody autreRb = collision.rigidbody;
-
-            autreRb.AddExplosionForce(repoussement, collision.contacts[0].point, 5);
-        
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Sol"))
+        {
+            isOnGround = false;
         }
     }
 
