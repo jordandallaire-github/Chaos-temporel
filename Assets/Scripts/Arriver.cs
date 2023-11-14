3729011ea0eaf96e58cd6ea57ff2b9c1abbe8272
shaynefortier <109255560+shaynefortier@users.noreply.github.com>
2023-11-14 11:32:00 -0500

    Final Ranking into ScriptableObjects

--- Assets/Scripts/Arriver.cs
+++ Assets/Scripts/Arriver.cs
@@ -6,20 +6,25 @@ using UnityEngine;
 public class Arriver : MonoBehaviour
 {
     private int index = 0;
-    private GameObject[] ranking;
+    public Rankings rankings;
+
     // Start is called before the first frame update
     void Start()
     {
-        ranking = new GameObject[4];
         
     }
 
     void OnTriggerEnter(Collider vehicule){
-        if (vehicule.tag == "Player" || vehicule.tag == "Adversaire")
+    if (vehicule.tag == "Player" || vehicule.tag == "Adversaire")
+    {
+        if (System.Array.IndexOf(rankings.ranking, vehicule.gameObject) == -1)
         {
-            Debug.Log(vehicule + " est à la place " + (index + 1) );
-            ranking[index] = vehicule.gameObject;
+            Debug.Log(vehicule.gameObject.name + " est à la place " + (index + 1) );
+            rankings.ranking[index] = vehicule.gameObject.name;
+            Debug.Log(vehicule.gameObject.GetType());
+            Debug.Log(rankings.ranking[index].GetType());
             index++;
         }
     }
+}
 }
