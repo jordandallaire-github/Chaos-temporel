4351e3fe1625313461f38c135b3e4463a6cbe732
Shayne Fortier <109255560+shaynefortier@users.noreply.github.com>
2023-12-17 17:39:06 -0500

    Change SerialPorts in Debug Screen

--- Assets/Scripts/Debug/DebugScreen.cs
+++ Assets/Scripts/Debug/DebugScreen.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DebugScreen : MonoBehaviour
 {
+    [SerializeField] private Configs configurations;
+    [SerializeField] private TMP_InputField J1Port;
+    [SerializeField] private TMP_InputField J2Port;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        J1Port.text = configurations.J1Port;
+        J2Port.text = configurations.J2Port;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // When a field is changed
+    public void OnTextChangedP1(string newPort){
+        configurations.J1Port = newPort;
+    }
+    public void OnTextChangedP2(string newPort){
+        configurations.J2Port = newPort;
     }
 }
