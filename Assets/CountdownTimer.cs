4c9ad7866d4333f2bfe53c5ba6c714592267e6cb
iVoltion <59234182+iVoltion@users.noreply.github.com>
2023-12-18 23:01:18 -0500

    Fix countdown

--- Assets/CountdownTimer.cs
+++ Assets/CountdownTimer.cs
@@ -1,30 +1,31 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CountdownTimer : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer;
-    public Sprite[] sprites;
+    public Image image;
+   public Sprite[] sprites;
+   
+   void Start()
+   {
+       StartCoroutine(Countdown(4)); // start a 10 second countdown
+   }
+   
+   public IEnumerator Countdown(int seconds)
+   {
+       int counter = seconds;
+       int spriteIndex = 0;
+       while (counter > 0)
+       {
+           Debug.Log("Counter: " + counter);
+           image.sprite = sprites[spriteIndex];
+           spriteIndex = (spriteIndex + 1) % sprites.Length; // cycle through sprites
+           yield return new WaitForSeconds(1);
+           counter--;
+       }
 
-    void Start()
-    {
-        StartCoroutine(Countdown(3)); // start a 10 second countdown
-    }
-
-    public IEnumerator Countdown(int seconds)
-    {
-        int counter = seconds;
-        int spriteIndex = 0;
-        while (counter > 0)
-        {
-            spriteRenderer.sprite = sprites[spriteIndex];
-            spriteIndex = (spriteIndex + 1) % sprites.Length; // cycle through sprites
-            yield return new WaitForSeconds(1);
-            counter--;
-        }
-
-        // Deactivate the GameObject when the countdown finishes
-        gameObject.SetActive(false);
-    }
-
+       // Deactivate the GameObject when the countdown finishes
+       gameObject.SetActive(false);
+   }
 }
